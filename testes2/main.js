ae1434aa9f52ae16fe98a9acdc1520c748ad5d43
Ryan <shippudenex@gmail.com>
2023-05-15 16:54:10 -0300

    update

--- testes2/main.js
+++ testes2/main.js
@@ -1,10 +1,25 @@
-function calcularMedia() {
-    var num1 = parseFloat(document.getElementById('num1').value);
-    var num2 = parseFloat(document.getElementById('num2').value);
-    var num3 = parseFloat(document.getElementById('num3').value);
-  
-    var media = (num1 + num2 + num3) / 3;
-  
-    document.getElementById('resultado').innerHTML = "A média é: " + media.toFixed(2);
-  }
-  
+const form = document.getElementById('form_atividade');
+const imgAprovado = '<img src="/Midias/aprovado.png" alt="Aprovado">"';
+const imgReprovado = '<img src="/Midias/reprovado.png" alt="Reprovado">';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome_atividade');
+    const inputNotaAtividade = document.getElementById('nota_atividade');
+
+    // alert(`Atividade: ${inputNomeAtividade.value}) - Nota: $(inputNotaAtividade.value)
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade >= 7 ? 'Aprovado': 'Reprovado'}</td>`;
+    linha += '</tr>';
+    linha += linha;
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linha;
+
+    inputNomeAtividade.value = ' ';
+    inputNomeAtividade.value = ' ';
+})
