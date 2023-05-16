ac1ff2a74c0862745886f4e5f2a6f4d46e80cd26
Ryan <shippudenex@gmail.com>
2023-05-16 17:11:46 -0300

    update

---
+++ Projeto_2/main.js
@@ -0,0 +1,29 @@
+main.js
+
+
+const formContato = document.getElementById("form_contato");
+
+
+const tabelaContatos = document.querySelector("table tbody");
+
+
+formContato.addEventListener("submit", function (event) {
+event.preventDefault(); 
+
+const nome = document.getElementById("nome_contato").value;
+const email = document.getElementById("email_contato").value;
+const telefone = document.getElementById("telefone_contato").value;
+
+const novaLinha = document.createElement("tr");
+novaLinha.innerHTML = `
+    <td>${nome}</td>
+    <td>${email}</td>
+    <td>${telefone}</td>
+`;
+
+
+tabelaContatos.appendChild(novaLinha);
+
+
+formContato.reset();
+});
