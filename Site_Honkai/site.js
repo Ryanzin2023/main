b65987539752032d21a9c46a9fafe0796b0ad40d
Ryan <shippudenex@gmail.com>
2023-05-18 17:03:37 -0300

    update

---
+++ Site_Honkai/site.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    var form = document.querySelector("form");
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    var messageInput = document.getElementById("message");
+
+
+    form.addEventListener("submit", function(event) {
+event.preventDefault(); 
+
+
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var message = messageInput.value;
+
+
+    console.log("Nome:", name);
+    console.log("Email:", email);
+    console.log("Mensagem:", message);
+
+
+    nameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+    });
+});
