aceb20599da32be00070c43b1e908dd5757bf5b5
Ryan <shippudenex@gmail.com>
2023-05-23 16:05:00 -0300

    update

--- Site_Honkai/site.js
+++ Site_Honkai/site.js
@@ -25,3 +25,15 @@ event.preventDefault();
     messageInput.value = "";
     });
 });
+
+$(document).ready(function(){
+    $('#carrossel_imagens').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000,
+    });
+
+    $('.menu_hamburger').click(function(){
+        $('nav').slideToggle();
+    })
+})
